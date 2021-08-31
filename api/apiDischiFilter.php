7e59a4caa85a9fb99efa0a1479f57b7f01a28895
emmepi92 <mariapiacoccioli@live.it>
2021-08-31 12:52:30 +0200

    bonus 1 swiffer

--- api/apiDischiFilter.php
+++ api/apiDischiFilter.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/../database/dischiDB.php";
 $genre =$_GET["genre"];
 
 $filteredDischi = array_filter($dischiDB, function($disco) use ($genre) {
-    return strtolower($disco['genre']) === strtolower($genre);
+    return strtolower($disco['genre']) === $genre;
 });
 
 // var_dump($filteredDischi);
